dea1acfe5ae6eab7b1ab14a067ff344553f3a840
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-10-20 22:38:23 -0300

    file structure.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res)=>{
