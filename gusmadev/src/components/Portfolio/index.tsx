5139711db5919aca65eca8441cf2f37455c6ded6
Arthur Gusmao <arthur.cabj@hotmail.com>
2021-04-24 19:32:25 -0300

    Mock version of blog posts created.

--- gusmadev/src/components/Portfolio/index.tsx
+++ gusmadev/src/components/Portfolio/index.tsx
@@ -11,6 +11,22 @@ export function Portfolio() {
           <h1>My Projects</h1>
           <p>Here you'll see a list with my latest projects. Check it out!</p>
           <ul>
+          <li>
+              <p>TradeMap Ações</p>
+              <button className={`${styles.greenButton} button`}>
+                <a target='_blank' href='https://trademap-acoes.vercel.app/'>
+                  <ImEnter /> Acess
+                </a>
+              </button>
+              <button className={`${styles.grayButton} button`}>
+                <a
+                  target='_blank'
+                  href='https://github.com/arthurgusma/desafio-frontend'
+                >
+                  <ImGithub /> Github
+                </a>
+              </button>
+            </li>
             <li>
               <p>Quem Paga a Conta?</p>
               <button className={`${styles.greenButton} button`}>
@@ -30,23 +46,7 @@ export function Portfolio() {
                 </a>
               </button>
             </li>
-            <li>
-              <p>TradeMap Ações</p>
-              <button className={`${styles.greenButton} button`}>
-                <a target='_blank' href='https://trademap-acoes.vercel.app/'>
-                  <ImEnter /> Acess
-                </a>
-              </button>
-              <button className={`${styles.grayButton} button`}>
-                <a
-                  target='_blank'
-                  href='https://github.com/arthurgusma/desafio-frontend'
-                >
-                  <ImGithub /> Github
-                </a>
-              </button>
-            </li>
-          </ul>
+            </ul>
         </div>
         <div className={styles.carousel}>
           <MyCarousel />
